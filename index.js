d1c3ee816a05f59518aaec9b8e4c865f8553776b
Jaime <jaheaga@gmail.com>
2023-02-14 14:16:10 -0500

    Implementación body parser

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.listen(3000, () => {
     console.log(`Corriendo en puerto ${3000}...`)

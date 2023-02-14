c8c5c2d54c694af0ab0db1adeaa1341b85407037
Jaime Aguilar Carrillo <aguilarcarrillo.11@teleperformance.co>
2023-02-14 14:16:33 -0500

    Implementación de CORS

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 app.listen(3000, () => {
     console.log(`Corriendo en puerto ${3000}...`)

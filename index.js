a228889d7c8e4fb686abc9bed982b2e5fea47723
Jaime Aguilar Carrillo <aguilarcarrillo.11@teleperformance.co>
2023-02-16 16:16:48 -0500

    Instalación librerias y creación endpoint root

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 const cors = require('cors');
 app.use(cors());
+
+app.use('/', require('./api/routes/index'));
 
 app.listen(3000, () => {
     console.log(`Corriendo en puerto ${3000}...`)
